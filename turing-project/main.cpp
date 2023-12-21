e6b274dd00872428843370ba870d0acd1c0399b6
Pecco <pecco@qq.com>
2023-12-21 23:52:38 +0800

    refactor: rewrite main function with error handler

--- turing-project/main.cpp
+++ turing-project/main.cpp
@@ -1,53 +1,38 @@
-#include "constants.h"
+#include "error.h"
 #include "tm.h"
-#include <iostream>
-
-void usage() {
-    std::cerr << "Usage: turing [-v|--verbose] [-h|--help] <tm_path> <input>"
-              << std::endl;
-}
+#include "util.h"
 
 int main(int argc, char *argv[]) {
     std::string tm_path, input;
     bool verbose = false;
-    for (int i = 1; i < argc; ++i) {
-        std::string arg(argv[i]);
-        if (arg == "-h" || arg == "--help") {
-            usage();
-            exit(0);
-        } else if (arg == "-v" || arg == "--verbose") {
-            verbose = true;
-        } else if (arg.size() > 1 && arg[0] == '-') {
-            std::cerr << ERR << "Invalid option: " << arg << std::endl;
-            std::cerr << NOTE;
-            usage();
-            exit(1);
-        } else if (tm_path.empty()) {
-            tm_path = arg;
-        } else if (input.empty()) {
-            input = arg;
-        } else {
-            std::cerr << ERR << "Too many arguments" << std::endl;
-            std::cerr << NOTE;
-            usage();
-            exit(1);
+    try {
+        for (int i = 1; i < argc; ++i) {
+            std::string arg(argv[i]);
+            if (arg == "-h" || arg == "--help") {
+                Util::usage();
+                exit(0);
+            } else if (arg == "-v" || arg == "--verbose") {
+                verbose = true;
+            } else if (arg.size() > 1 && arg[0] == '-') {
+                throw ArgError{ArgError::Type::INVALID_OPTION, arg};
+            } else if (tm_path.empty()) {
+                tm_path = arg;
+            } else if (input.empty()) {
+                input = arg;
+            } else {
+                throw ArgError{ArgError::Type::TOO_MANY_ARGS, arg};
+            }
         }
+        if (input.empty()) {
+            throw ArgError{ArgError::Type::TOO_FEW_ARGS, ""};
+        }
+        TuringMachine tm;
+        tm.parse(tm_path);
+        tm.run(input, verbose);
+    } catch (ArgError &e) {
+        e.log();
+    } catch (CodeError &e) {
+        e.log(verbose);
     }
-    if (tm_path.empty()) {
-        std::cerr << ERR << "No tm_path given" << std::endl;
-        std::cerr << NOTE;
-        usage();
-        exit(1);
-    } else if (input.empty()) {
-        std::cerr << ERR << "No input given" << std::endl;
-        std::cerr << NOTE;
-        usage();
-        exit(1);
-    }
-
-    TuringMachine tm;
-    tm.parse(tm_path, verbose);
-    tm.run(input, verbose);
-
     return 0;
 }
