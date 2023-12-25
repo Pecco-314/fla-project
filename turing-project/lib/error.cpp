5ac3c77df1b7a3c3147c5914c6cb486cd3d3cdc5
Pecco <pecco@qq.com>
2023-12-25 23:51:24 +0800

    feat: enhance error messages with bold font and location indicator

--- turing-project/lib/error.cpp
+++ turing-project/lib/error.cpp
@@ -4,15 +4,16 @@
 #include <iostream>
 
 void ArgError::log() const {
+    std::cerr << ERR;
     switch (type) {
     case Type::INVALID_OPTION:
-        std::cerr << ERR << "Invalid option: " << arg << std::endl;
+        std::cerr << "Invalid option: " << arg << std::endl;
         break;
     case Type::TOO_MANY_ARGS:
-        std::cerr << ERR << "Too many arguments" << std::endl;
+        std::cerr << "Too many arguments" << std::endl;
         break;
     case Type::TOO_FEW_ARGS:
-        std::cerr << ERR << "Too few arguments" << std::endl;
+        std::cerr << "Too few arguments" << std::endl;
         break;
     }
     std::cerr << NOTE;
@@ -22,61 +23,61 @@ void ArgError::log() const {
 
 void CodeError::log(bool verbose) const {
     if (verbose) {
+        std::cerr << BOLD << span.code->path() << ":" << span.st_lno + 1 << ":" << span.st_cno + 1 << ": " << ERR;
         switch (type) {
         case Type::LEXER_INVALID_CHAR:
-            std::cerr << ERR << "Invalid character: " << Util::quoted(*span.begin()) << std::endl;
+            std::cerr << "Invalid character: " << Util::quoted(*span.begin()) << std::endl;
             break;
         case Type::PARSER_EXPECTED_Q:
-            std::cerr << ERR << "Expected #Q" << std::endl;
+            std::cerr << "Expected #Q" << std::endl;
             break;
         case Type::PARSER_EXPECTED_S:
-            std::cerr << ERR << "Expected #S" << std::endl;
+            std::cerr << "Expected #S" << std::endl;
             break;
         case Type::PARSER_EXPECTED_G:
-            std::cerr << ERR << "Expected #G" << std::endl;
+            std::cerr << "Expected #G" << std::endl;
             break;
         case Type::PARSER_EXPECTED_Q0:
-            std::cerr << ERR << "Expected #q0" << std::endl;
+            std::cerr << "Expected #q0" << std::endl;
             break;
         case Type::PARSER_EXPECTED_B:
-            std::cerr << ERR << "Expected #B" << std::endl;
+            std::cerr << "Expected #B" << std::endl;
             break;
         case Type::PARSER_EXPECTED_EQUAL:
-            std::cerr << ERR << "Expected '='" << std::endl;
+            std::cerr << "Expected '='" << std::endl;
             break;
         case Type::PARSER_EXPECTED_LBRACE:
-            std::cerr << ERR << "Expected '{'" << std::endl;
+            std::cerr << "Expected '{'" << std::endl;
             break;
         case Type::PARSER_EXPECTED_ID:
-            std::cerr << ERR << "Expected identifier" << std::endl;
+            std::cerr << "Expected identifier" << std::endl;
             break;
         case Type::PARSER_UNCLOSED_SET:
-            std::cerr << ERR << "Unclosed set" << std::endl;
+            std::cerr << "Unclosed set" << std::endl;
             break;
         case Type::PARSER_EXPECTED_VALID_CHAR:
-            std::cerr << ERR << "Expected valid character" << std::endl;
-            std::cerr << NOTE << "Valid characters are: ASCII graph characters except for `,;{}*`"<< std::endl;
+            std::cerr << "Expected valid character (valid characters are: ASCII graph characters except for `,;{}*`)"<< std::endl;
             break;
         case Type::PARSER_UNEXPECTED_UNDERSCORE:
-            std::cerr << ERR << "Input alphabet cannot contain underscore" << std::endl; 
+            std::cerr << "Input alphabet cannot contain underscore" << std::endl; 
             break;
         case Type::PARSER_EMPTY_SET:
-            std::cerr << ERR << "Set is empty" << std::endl;
+            std::cerr << "Set is empty" << std::endl;
             break;
         case Type::PARSER_EXPECTED_UNDERSCORE_AS_BLANK:
-            std::cerr << ERR << "Expected underscore as blank character" << std::endl;
+            std::cerr << "Expected underscore as blank character" << std::endl;
             break;
         case Type::VALIDATOR_MISSING_UNDERSCORE_IN_G:
-            std::cerr << ERR << "Tape alphabet must contain underscore" << std::endl;
+            std::cerr << "Tape alphabet must contain underscore" << std::endl;
             break;
         case Type::VALIDATOR_S_NOT_SUBSET_OF_G:
-            std::cerr << ERR << "Input alphabet must be a subset of tape alphabet" << std::endl;
+            std::cerr << "Input alphabet must be a subset of tape alphabet" << std::endl;
             break;
         case Type::VALIDATOR_INVALID_INITIAL_STATE:
-            std::cerr << ERR << "Initial state must be a valid state" << std::endl;
+            std::cerr << "Initial state must be a valid state" << std::endl;
             break;
         }
-        span.code->printHighlight(span, RED);
+        span.code->printHighlight(span, RED BOLD);
     } else {
         std::cerr << "Syntax error" << std::endl;
     }
