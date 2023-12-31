6b7676fb1fdc572ff240115e3e6cc9cb62f9e7bd
Pecco <pecco@qq.com>
2023-12-31 20:05:20 +0800

    feat: implement util::format

--- turing-project/include/util.h
+++ turing-project/include/util.h
@@ -1,11 +1,63 @@
 #ifndef __UTIL_H__
 #define __UTIL_H__
 
-#include <string>
+#include <sstream>
 
 namespace util {
 
 std::string quoted(char ch);
+
+inline std::string format(std::string_view fmt) {
+    std::string s;
+    for (int i = 0; i < (int)fmt.size(); i++) {
+        if (fmt[i] == '{') {
+            if (fmt[i + 1] == '{') {
+                s += fmt[i];
+                i++;
+            } else {
+                throw std::runtime_error("format error");
+            }
+        } else if (fmt[i] == '}') {
+            if (fmt[i + 1] == '}') {
+                s += fmt[i];
+                i++;
+            } else {
+                throw std::runtime_error("format error");
+            }
+        } else {
+            s += fmt[i];
+        } 
+    }
+    return s;
+}
+
+template <typename... Args, typename T>
+std::string format(std::string_view fmt, const T &t, Args... args) {
+    std::stringstream ss;
+    for (int i = 0; i < (int)fmt.size(); i++) {
+        if (fmt[i] == '{') {
+            if (fmt[i + 1] == '{') {
+                ss << fmt[i];
+                i++;
+            } else if (fmt[i + 1] == '}') {
+                ss << t << format(fmt.substr(i + 2), args...);
+                break;
+            } else {
+                throw std::runtime_error("format error");
+            }
+        } else if (fmt[i] == '}') {
+            if (fmt[i + 1] == '}') {
+                ss << fmt[i];
+                i++;
+            } else {
+                throw std::runtime_error("format error");
+            }
+        } else {
+            ss << fmt[i];
+        }
+    }
+    return ss.str();
+}
 
 } // namespace util
 
