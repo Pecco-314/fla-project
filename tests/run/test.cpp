158c0d1735e180b33071751197ba4c13f0526c2f
Pecco <pecco@qq.com>
2024-01-02 23:33:38 +0800

    test: create test_run

---
+++ tests/run/test.cpp
@@ -0,0 +1,69 @@
+#include "color.h"
+#include "util.h"
+#include <filesystem>
+#include <fstream>
+#include <iostream>
+
+std::filesystem::path test_dir = "../tests/run";
+std::filesystem::path tm_dir = test_dir / "tm";
+std::filesystem::path out_dir = test_dir / "out";
+std::filesystem::path temp_dir = std::filesystem::temp_directory_path();
+std::filesystem::path turing = util::findFileWithPrefix("turing");
+
+void test(std::string_view name, std::string_view input, bool verbose = false,
+          int expected_code = 0) {
+    std::string testname =
+        std::string(name) + "_" + std::string(input) + (verbose ? "_verbose" : "");
+    auto tm_file = tm_dir / name += ".tm";
+    auto expected_file = out_dir / testname += ".txt";
+    auto actual_file = temp_dir / "actual.txt";
+    auto debug_file = temp_dir / "debug.txt";
+    auto out = expected_code ? debug_file : actual_file;
+    auto err = expected_code ? actual_file : debug_file;
+    auto code = util::runCommand(turing, out, err,
+                                 {tm_file.string(), input, verbose ? "-v" : ""});
+    TermColor::setForceColor();
+    if (expected_code != code) {
+        std::cerr << FAILED << testname << std::endl;
+        std::cerr << "expected code: " << expected_code << std::endl;
+        std::cerr << "  actual code: " << code << std::endl;
+        std::cerr << (expected_code ? "stderr" : "stdout") << ":" << std::endl;
+        util::runCommand("cat", "", "", {debug_file.string()});
+        exit(1);
+    }
+
+    std::ifstream expected(expected_file);
+    std::ifstream actual(actual_file);
+    for (int i = 0;; i++) {
+        std::string expected_line, actual_line;
+        std::getline(expected, expected_line);
+        std::getline(actual, actual_line);
+        if (!expected && !actual) { break; }
+        if (!expected) {
+            expected_line = "<EOF>";
+        } else {
+            expected_line = util::strip(expected_line);
+        }
+        if (!actual) {
+            actual_line = "<EOF>";
+        } else {
+            actual_line = util::strip(actual_line);
+        }
+        if (expected_line != actual_line) {
+            std::cerr << FAILED << testname << std::endl;
+            std::cerr << "(in line " << i + 1 << ")" << std::endl;
+            std::cerr << "expected: " << expected_line << std::endl;
+            std::cerr << "  actual: " << actual_line << std::endl;
+            std::cerr << (expected_code ? "stderr" : "stdout") << ":" << std::endl;
+            util::runCommand("cat", "", "", {debug_file.string()});
+            exit(1);
+        }
+    }
+    std::cerr << PASSED << testname << std::endl;
+    TermColor::setForceColor(false);
+}
+
+int main() {
+    test("palindrome_detector_2tapes", "1001001", true);
+    return 0;
+}
